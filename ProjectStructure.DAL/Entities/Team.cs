7f523ed4a03c0da8a8338a4875e16be02051d8d4
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 14:06:17 +0200

    add entities

---
+++ ProjectStructure.DAL/Entities/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectStructure.DAL.Entities
+{
+    public class Team : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+    }
+}
---
+++ ProjectStructure.DAL/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectStructure.DAL.Entities
+{
+    public class User : BaseEntity
+    {
+        public int? TeamId { get; set; }
+        public Team Team { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public DateTime BirthDay { get; set; }
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+        public ICollection<Task> Tasks { get; set; } = new List<Task>();
+    }
+}
