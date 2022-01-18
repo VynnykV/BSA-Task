f799b110e2103b04ae17f0d257c3ef258abdde70
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 18:38:28 +0200

    add queryService and menu

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,8 +13,199 @@ namespace Client
             ServiceCollection serviceDescriptors = new ServiceCollection();
             ConfigureServices(serviceDescriptors);
             IServiceProvider serviceProvider = serviceDescriptors.BuildServiceProvider();
+            var queryService = serviceProvider.GetService<QueryService>();
+            DisplayMainMenu();
+            MainMenu(queryService);
         }
 
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("Welcome to LINQ!\n\n" +
+                              "Enter a number what have to do:\n" +
+                              "1. Get the number of tasks in the project of a particular user (by id).\n" +
+                              "2. Get a list of tasks designed for a specific user (by id), where the name tag <45 characters.\n" +
+                              "3. Get a list (id, name) from the collection of tasks completed (completed) in the current (2021) year for a specific user (by id).\n" +
+                              "4. Get a list (id, team name and user list) from the collection of teams over 10 years old, sorted for this burning user registration, and grouped by team.\n" +
+                              "5. Get a list of users in alphabetical order first_name (ascending) with sorted tasks by length name (descending).\n" +
+                              "6. Get user info by id.\n" +
+                              "7. Get project info by id\n" +
+                              "8. Clear console.\n" +
+                              "9. Exit program.\n");
+        }
+
+        static void MainMenu(QueryService queryService)
+        {
+
+            Console.Write("Your choice: ");
+            string choice = Console.ReadLine();
+            try
+            {
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.Write("Enter userId: ");
+                            int userId;
+                            while (!int.TryParse(Console.ReadLine(), out userId))
+                            {
+                                Console.WriteLine("Wrong id");
+                                Console.Write("Enter userId: ");
+                            }
+
+                            var result = queryService.TasksInProjectByUserCount(userId).Result;
+                            foreach (var r in result)
+                            {
+                                Console.WriteLine($"Project:\n{r.Project}");
+                                Console.WriteLine($"Has {r.NumsOfTasks} tasks");
+                                Console.WriteLine("-------------------------------------");
+                            }
+                        }
+
+                        break;
+
+                    case "2":
+                        {
+                            Console.Write("Enter userId: ");
+                            int userId;
+                            while (!int.TryParse(Console.ReadLine(), out userId))
+                            {
+                                Console.WriteLine("Wrong id");
+                                Console.Write("Enter userId: ");
+                            }
+
+                            var result = queryService.GetTasksLimitedByName(userId, 45).Result;
+                            foreach (var task in result)
+                            {
+                                Console.WriteLine($"Task:\n{task}");
+                            }
+
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            Console.Write("Enter userId: ");
+                            int userId;
+                            while (!int.TryParse(Console.ReadLine(), out userId))
+                            {
+                                Console.WriteLine("Wrong id");
+                                Console.Write("Enter userId: ");
+                            }
+
+                            var result = queryService.GetFinishedTasks(userId, 2021).Result;
+                            foreach (var r in result)
+                            {
+                                Console.WriteLine($"id: {r.Id} name: {r.Name}");
+                            }
+
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            var result = queryService.GetOlderUsers(10).Result;
+                            foreach (var r in result)
+                            {
+                                Console.WriteLine($"id: {r.Id} name: {r.Name}");
+                                Console.WriteLine("-------------------------------------\nUsers:");
+                                foreach (var user in r.Users)
+                                {
+                                    Console.WriteLine(user);
+                                }
+
+                                Console.WriteLine("-------------------------------------");
+                            }
+
+                            break;
+                        }
+
+                    case "5":
+                        {
+                            var result = queryService.GetSortedUsersWithTasks().Result;
+                            foreach (var r in result)
+                            {
+                                Console.WriteLine("-------------------------------------");
+                                Console.WriteLine($"User:\n{r.User}");
+                                Console.WriteLine("-------------------------------------");
+                                Console.WriteLine("User's tasks");
+                                foreach (var task in r.Tasks)
+                                {
+                                    Console.WriteLine(task);
+                                }
+                            }
+                            break;
+                        }
+
+                    case "6":
+                        {
+                            Console.Write("Enter userId: ");
+                            int userId;
+                            while (!int.TryParse(Console.ReadLine(), out userId))
+                            {
+                                Console.WriteLine("Wrong id");
+                                Console.Write("Enter userId: ");
+                            }
+                            var result = queryService.GetUserInfo(userId).Result;
+                            if(result is not null)
+                            {
+                                Console.WriteLine($"user:\n{result.User}\n" +
+                                              $"last project:\n{result.LastProject}\n" +
+                                              $"tasks quantity int last project: {result.TasksQuantityLastProject}\n" +
+                                              $"general quantity of cancelled tasks: {result.GeneralQuantityCancelledTasks}\n" +
+                                              $"general quantity of unfinished tasks: {result.GeneralQuantityUnfinishedTasks}\n" +
+                                              $"the longest task by duration:\n {result.LongestTaskByDuration}");
+                            }
+                            else
+                                Console.WriteLine("No information about this user.");
+                            break;
+                        }
+
+                    case "7":
+                        {
+                            Console.Write("Enter projectId: ");
+                            int projectId;
+                            while (!int.TryParse(Console.ReadLine(), out projectId))
+                            {
+                                Console.WriteLine("Wrong id");
+                                Console.Write("Enter userId: ");
+                            }
+                            var result = queryService.GetProjectInfo(projectId).Result;
+                            if(result is not null)
+                            {
+                                Console.WriteLine($"project:\n{result.Project}\n" +
+                                              $"the longest task by desc:\n {result.LongestProjectTaskByDesc}\n" +
+                                              $"the shortest task by name:\n {result.ShortestProjectTaskByName}\n" +
+                                              $"The total number of users in the project team, where either the project description> 20 characters, or the number of tasks <3:" +
+                                              $" {result.TeamUsersCount}");
+                            }
+                            else
+                                Console.WriteLine("No information about this project");
+                            break;
+                        }
+
+                    case "8":
+                        Console.Clear();
+                        break;
+
+                    case "9":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Entered incorrect number, try again");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                DisplayMainMenu();
+                MainMenu(queryService);
+            }
+        }
 
         private static void ConfigureServices(IServiceCollection serviceCollection)
         {
@@ -24,6 +215,8 @@ namespace Client
                 .Build();
 
             serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+            serviceCollection.AddHttpClient<QueryService>();
+            serviceCollection.AddTransient<QueryService>();
         }
     }
 }
---
+++ Client/QueryService.cs
@@ -0,0 +1,75 @@
+﻿using Client.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Task = Client.ViewModels.Task;
+
+namespace Client
+{
+    public class QueryService
+    {
+        private readonly IConfigurationRoot _config;
+        private readonly HttpClient _httpClient;
+        private readonly string baseUrl;
+
+        public QueryService(
+            IConfigurationRoot config,
+            HttpClient httpClient)
+        {
+            _config = config;
+            _httpClient = httpClient;
+            baseUrl = _config.GetSection("Url").Value;
+        }
+
+        public async Task<IEnumerable<ProjectCountTasksDTO>> TasksInProjectByUserCount(int userId)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"tasksQuantityInProject/{userId}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<ProjectCountTasksDTO>>(json);
+        }
+
+        public async Task<IEnumerable<Task>> GetTasksLimitedByName(int userId, int symbolsQuantity)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"tasksLimitedByName/{userId}/{symbolsQuantity}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<Task>>(json);
+        }
+
+        public async Task<IEnumerable<FinishedTask>> GetFinishedTasks(int userId, int year)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"finishedTasks/{userId}/{year}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<FinishedTask>>(json);
+        }
+
+        public async Task<IEnumerable<TeamUsersDTO>> GetOlderUsers(int age)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"olderUsers/{age}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<TeamUsersDTO>>(json);
+        }
+
+        public async Task<IEnumerable<UserWithTasksDTO>> GetSortedUsersWithTasks()
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"sortedUsersWithTasks");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<UserWithTasksDTO>>(json);
+        }
+
+        public async Task<UserInfo> GetUserInfo(int userId)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"getUserInfo/{userId}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<UserInfo>(json);
+        }
+
+        public async Task<ProjectInfo> GetProjectInfo(int projectId)
+        {
+            var response = await _httpClient.GetAsync(baseUrl + $"getProjectInfo/{projectId}");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ProjectInfo>(json);
+        }
+    }
+}
