eb70b67572725b8e37f8616a263e52fb5e0712ac
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 15:43:33 +0200

    add custom exception filter

---
+++ ProjectStructure.WebAPI/Extensions/ExceptionFilterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using ProjectStructure.BLL.Exceptions;
+
+namespace ProjectStructure.WebAPI.Extensions
+{
+    public static class ExceptionFilterExtensions
+    {
+        public static HttpStatusCode ParseException(this Exception exception)
+        {
+            return exception switch
+            {
+                NotFoundException _ => HttpStatusCode.NotFound,
+                ArgumentException _ => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.InternalServerError
+            };
+        }
+    }
+}
