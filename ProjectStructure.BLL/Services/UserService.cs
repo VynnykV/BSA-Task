8c625acbae29c4c9238bad3ad71831a9286df6e4
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 15:05:20 +0200

    add services

---
+++ ProjectStructure.BLL/Services/UserService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using ProjectStructure.BLL.Exceptions;
+using ProjectStructure.BLL.Interfaces;
+using ProjectStructure.Common.DTO.User;
+using ProjectStructure.DAL.Entities;
+using ProjectStructure.DAL.Interfaces;
+
+namespace ProjectStructure.BLL.Services
+{
+    public class UserService : BaseService, IUserService
+    {
+        public UserService(
+            IUnitOfWork unitOfWork,
+            IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        public UserDTO AddUser(UserCreateDTO user)
+        {
+            var userEntity = _mapper.Map<User>(user);
+            userEntity.RegisteredAt = DateTime.Now;
+            _unitOfWork.UserRepository.Create(userEntity);
+            return _mapper.Map<UserDTO>(userEntity);
+        }
+
+        public IEnumerable<UserDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<UserDTO>>(_unitOfWork.UserRepository.GetAll());
+        }
+
+        public UserDTO GetUserById(int id)
+        {
+            var userEntity = _unitOfWork.UserRepository.GetById(id);
+            if (userEntity is null)
+                throw new NotFoundException((nameof(User), id));
+            return _mapper.Map<UserDTO>(userEntity);
+        }
+
+        public void UpdateUser(UserDTO user)
+        {
+            var userEntity = _mapper.Map<User>(user);
+            if (_unitOfWork.UserRepository.GetById(user.Id) is null)
+                throw new NotFoundException((nameof(User), user.Id));
+            _unitOfWork.UserRepository.Update(userEntity);
+        }
+
+        public void DeleteUser(int id)
+        {
+            var userEntity = _unitOfWork.UserRepository.GetById(id);
+            if (userEntity is null)
+                throw new NotFoundException((nameof(User), id));
+            _unitOfWork.UserRepository.Delete(id);
+        }
+    }
+}
