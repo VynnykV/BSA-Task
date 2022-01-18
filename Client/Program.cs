ae3474af0d0fd18354348a9114352a2c7e872446
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 17:01:24 +0200

    configure console application

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+        static void Main(string[] args)
+        {
+            ServiceCollection serviceDescriptors = new ServiceCollection();
+            ConfigureServices(serviceDescriptors);
+            IServiceProvider serviceProvider = serviceDescriptors.BuildServiceProvider();
+        }
+
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+        }
+    }
+}
