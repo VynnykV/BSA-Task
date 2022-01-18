6bda3d005183c2d8d49d60e0616137ff8d7a7842
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 14:17:08 +0200

    add DAL abstractions

---
+++ ProjectStructure.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ProjectStructure.DAL.Entities;
+
+namespace ProjectStructure.DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        TEntity Create(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(int id);
+    }
+}
---
+++ ProjectStructure.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using ProjectStructure.DAL.Entities;
+
+namespace ProjectStructure.DAL.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<User> UserRepository { get; }
+        IRepository<Project> ProjectRepository { get; }
+        IRepository<Task> TaskRepository { get; }
+        IRepository<Team> TeamRepository { get; }
+    }
+}
