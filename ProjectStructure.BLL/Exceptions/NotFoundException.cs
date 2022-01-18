2f902eac0e5ffe7a5cb06acd8ac08f02ec4a1627
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 14:42:04 +0200

    add not found exception

---
+++ ProjectStructure.BLL/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectStructure.BLL.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, int id)
+            : base($"Entity {name} with id ({id}) was not found.")
+        {
+        }
+
+        public NotFoundException((string, int id) name) : base($"Entity {name} was not found.") { }
+    }
+}
