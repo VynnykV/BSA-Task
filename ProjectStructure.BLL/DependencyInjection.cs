914c4c98d77ea39a182a71ec5c1b0fd23af49122
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 15:08:43 +0200

    add dependencies to DI container

---
+++ ProjectStructure.BLL/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectStructure.BLL.Interfaces;
+using ProjectStructure.BLL.MappingProfiles;
+using ProjectStructure.BLL.Services;
+
+namespace ProjectStructure.BLL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<ITaskService, TaskService>();
+            services.AddScoped<ITeamService, TeamService>();
+            services.AddAutoMapper(typeof(UserProfile), typeof(ProjectProfile), typeof(TaskProfile), typeof(TeamProfile));
+            return services;
+        }
+    }
+}
