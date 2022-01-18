4cfffa43b5b20af52ad6596917d7283fff0b4f5f
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-18 14:20:49 +0200

    add dependencies to DI container

---
+++ ProjectStructure.DAL/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectStructure.DAL.Interfaces;
+using ProjectStructure.DAL.Repositories;
+
+namespace ProjectStructure.DAL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<DataContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
