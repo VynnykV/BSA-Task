091417277b0f68af6e5f0409873f739cd6bf17e5
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-20 22:41:40 +0200

    add ef

--- ProjectStructure.DAL/DependencyInjection.cs
+++ ProjectStructure.DAL/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using ProjectStructure.DAL.Interfaces;
 using ProjectStructure.DAL.Repositories;
 
@@ -6,9 +8,12 @@ namespace ProjectStructure.DAL
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        public static IServiceCollection AddRepositories(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
-            services.AddSingleton<DataContext>();
+            services.AddDbContext<DataContext>(opt =>
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             return services;
         }
