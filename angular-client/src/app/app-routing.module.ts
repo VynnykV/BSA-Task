b8a7ebc52480a603ac5994e480fc7770b1714687
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-01-30 15:16:58 +0200

    add home page

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GreetingComponent} from "./components/greeting/greeting.component";
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    { path: '', component: GreetingComponent, pathMatch: 'full' },
+    { path: '**', redirectTo: '' }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { GreetingComponent } from './components/greeting/greeting.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
