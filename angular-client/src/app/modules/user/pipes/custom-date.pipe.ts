24e775edcff611275d75f2ea878f565f41feabd0
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-02-01 20:31:36 +0200

    simplify pipe

--- angular-client/src/app/modules/user/pipes/custom-date.pipe.ts
+++ angular-client/src/app/modules/user/pipes/custom-date.pipe.ts
@@ -1,19 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {DatePipe, formatDate} from "@angular/common";
-
-const months = new Map<string, string>();
-months.set("January", "Січня");
-months.set("February", "Лютого");
-months.set("March", "Березня");
-months.set("April", "Квітня");
-months.set("May", "Травня");
-months.set("June", "Червня");
-months.set("July", "Липня");
-months.set("August", "Серпня");
-months.set("September", "Вересня");
-months.set("October", "Жовтня");
-months.set("November", "Листопада");
-months.set("December", "Грудня");
 
 @Pipe({
   name: 'customDate'
@@ -22,13 +7,9 @@ months.set("December", "Грудня");
 export class CustomDatePipe implements PipeTransform {
 
   transform(value: Date): string {
-    let date = new Date(value);
-    let month = date.toLocaleString('en', { month: 'long' });
-    let translatedMonth: string = months.get(month) || "";
-    let result = new DatePipe('en').transform(date, 'dd') + " " +
-      translatedMonth.toLowerCase() + " " +
-      new DatePipe('en').transform(date, 'yyyy');
-    return result;
+    value = new Date(value);
+    let date = value.toLocaleString('uk-UA', {year: 'numeric', month: 'long', day: 'numeric'});
+    return date;
   }
 
 }
